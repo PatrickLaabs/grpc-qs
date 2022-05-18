00c52f01db979d260d89f56439d7ca061be8adfe
PatrickLaabs <patrick.laabs@me.com>
2022-05-18 14:34:09 +0200

    Removed Printing status to stdout, since the logs are getting spammed

--- healthchecker/medium-health-checker.go
+++ healthchecker/medium-health-checker.go
@@ -2,7 +2,6 @@ package healthchecker
 
 import (
 	"context"
-	"fmt"
 	"google.golang.org/grpc/health/grpc_health_v1"
 )
 
@@ -10,7 +9,7 @@ type HealthChecker struct {
 }
 
 func (s *HealthChecker) Check(ctx context.Context, req *grpc_health_v1.HealthCheckRequest) (*grpc_health_v1.HealthCheckResponse, error) {
-	fmt.Println("Serving the Check request fir health check")
+	// fmt.Println("Serving the Check request for health check")
 
 	return &grpc_health_v1.HealthCheckResponse{
 		Status: grpc_health_v1.HealthCheckResponse_SERVING,
@@ -18,7 +17,7 @@ func (s *HealthChecker) Check(ctx context.Context, req *grpc_health_v1.HealthChe
 }
 
 func (s *HealthChecker) Watch(req *grpc_health_v1.HealthCheckRequest, server grpc_health_v1.Health_WatchServer) error {
-	fmt.Println("Serving the Watch request for health check")
+	// fmt.Println("Serving the Watch request for health check")
 	return server.Send(&grpc_health_v1.HealthCheckResponse{
 		Status: grpc_health_v1.HealthCheckResponse_SERVING,
 	})
